f87bc932e2a9e8315db601380e532810a860734d
David Leonardo Espinosa Ochoa <dlespinosa@icesi.edu.co>
2024-04-12 04:00:03 -0500

    feat: adding a login form component with user validation and updating login page

--- web-app/next-frontend/src/app/ui/login-form.tsx
+++ web-app/next-frontend/src/app/ui/login-form.tsx
@@ -1,6 +1,43 @@
-export function CredentialsForm() {
+'use client'
+
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface CredentialsFormProps {
+    csrfToken?: string;
+}
+
+export function CredentialsForm(props: CredentialsFormProps) {
+    const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+
+    const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const data = new FormData(e.currentTarget);
+
+        const signInResponse = await signIn("credentials", {
+            email: data.get("email"),
+            password: data.get("password"),
+            redirect: false,
+        });
+
+        if (signInResponse && !signInResponse.error) {
+            router.push("/dashboard");
+        } else {
+            console.log("Error: ", signInResponse);
+            setError("Your email or password is wrong!");
+        }
+    };
+
     return (
-        <form className="w-full max-w-md mx-auto mt-8 text-xl text-black font-semibold flex flex-col">
+        <form className="w-full max-w-md mx-auto mt-8 text-xl text-black font-semibold flex flex-col" onSubmit={handleSubmit}>
+
+            {error && (
+                <span className="p-4 mb-2 text-lg font-semibold text-white bg-red-500 rounded-md">
+                    {error}
+                </span>
+            )}
             <div className="mb-4">
                 <label htmlFor="email" className="block text-sm font-bold mb-1">Example</label>
                 <input
