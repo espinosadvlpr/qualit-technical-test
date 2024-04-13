5b033d09e7f0363c9fea15798588a67aa7b57b29
David Leonardo Espinosa Ochoa <dlespinosa@icesi.edu.co>
2024-04-13 00:04:35 -0500

    feat: adding login validation to dashboard and show username in nav and home page

--- web-app/next-frontend/src/app/dashboard/page.tsx
+++ web-app/next-frontend/src/app/dashboard/page.tsx
@@ -1,8 +1,13 @@
+import { authConfig, loginIsRequiredServer } from "../api/auth/[...nextauth]/route"
+import { getServerSession } from "next-auth/next"
+
 export default async function Dashboard() {
+    await loginIsRequiredServer()
+    const session = await getServerSession(authConfig)
     return (
         <>
             <div className="flex justify-center items-center w-full h-full">
-                <h1 className="mb-4 text-3xl font-bold md:text-4xl lg:text-2xl xl:text-3xl">Welcome to the products app!</h1>
+                <h1 className="mb-4 text-3xl font-bold md:text-4xl lg:text-2xl xl:text-3xl">Welcome @{session?.user?.name} to the products app!</h1>
             </div>
         </>
     )
