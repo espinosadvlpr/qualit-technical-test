eb2912048c313f9daccf026275211767becade14
David Leonardo Espinosa Ochoa <dlespinosa@icesi.edu.co>
2024-04-13 00:12:56 -0500

    fix: adding cors policy to the server

--- web-app/nest-backend/src/main.ts
+++ web-app/nest-backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn']
   });
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
